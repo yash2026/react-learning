f58a231a2f413eeee787954f6b633d67613be395
Yashpal <yash38709@gmail.com>
2024-03-17 00:41:28 +0530

    form inputs auth

--- form/src/components/SimpleInput.js
+++ form/src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useRef, useState } from "react";
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState();
+
+  const nameInputHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    if (enteredName.trim() === "") {
+      return;
+    }
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
